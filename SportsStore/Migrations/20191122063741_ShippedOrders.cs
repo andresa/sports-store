39dfb599a1c454624ec0dd03800d20ef4133bab5
Andres Araya <andres_araya@outlook.com>
2020-06-06 13:18:49 +1000

    initial commit

---
+++ SportsStore/Migrations/20191122063741_ShippedOrders.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SportsStore.Migrations
+{
+    public partial class ShippedOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartLine_Order_OrderID",
+                table: "CartLine");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Order",
+                table: "Order");
+
+            migrationBuilder.RenameTable(
+                name: "Order",
+                newName: "Orders");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Shipped",
+                table: "Orders",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders",
+                column: "OrderID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartLine_Orders_OrderID",
+                table: "CartLine",
+                column: "OrderID",
+                principalTable: "Orders",
+                principalColumn: "OrderID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CartLine_Orders_OrderID",
+                table: "CartLine");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Shipped",
+                table: "Orders");
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                newName: "Order");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Order",
+                table: "Order",
+                column: "OrderID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CartLine_Order_OrderID",
+                table: "CartLine",
+                column: "OrderID",
+                principalTable: "Order",
+                principalColumn: "OrderID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
